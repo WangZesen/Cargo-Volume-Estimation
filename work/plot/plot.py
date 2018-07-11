6fe417cdc5bea5608361b600973dcf17f09ea081
Bullet Zhang <crazybullet28zhang@gmail.com>
2018-07-11 11:48:28 +0800

    update with multiple device suport

--- work/plot/plot.py
+++ work/plot/plot.py
@@ -1,11 +1,12 @@
 import numpy as np
 import os
 import time
+import align_cloud
 from mayavi import mlab
 
 
-def readFile(idx):
-	dict = './../Save_Point_Cloud/0/'
+def readFile(device, idx):
+	dict = './../Save_Point_Cloud/'+str(device)+'/'
 	filename_log = dict+str(idx).zfill(3)+'_log.txt'
 	waiting = False
 	while not os.path.exists(filename_log):
@@ -22,6 +23,7 @@ def readFile(idx):
 	fp = open(filename_log, 'r')
 	cargo_idx = int(fp.readline())
 	image_num = int(fp.readline())
+	fp.close()
 	print "read in cargo " + str(idx)
 	'''
 	data = []
@@ -35,7 +37,7 @@ def readFile(idx):
 	data = np.load(filename)
 	#except:
 		#print "failure" + filename_log
-	return data, image_num
+	return data, image_num, idx
 
 
 if __name__ == '__main__':
@@ -46,22 +48,35 @@ if __name__ == '__main__':
 	scalars = -y
 	plt = mlab.points3d(x, y, z, scalars, colormap="RdYlBu", scale_factor=0.01, scale_mode='none')
 	
-	@mlab.animate(delay=10)
+	@mlab.animate(delay=50)
 	def anim(idx):
+		
 		while True:
-			rawData, image_num = readFile(idx)
-			x, y, z = np.array([[],[],[]])
-			for data in rawData:
-				x_new, y_new, z_new = np.transpose(data)
-				x = np.hstack([x, x_new])
-				y = np.hstack([y, y_new])
-				z = np.hstack([z, z_new])
-				scalars = -y
-				plt.mlab_source.reset(x=x, y=y, z=z, scalars=scalars)
-				yield
+			for device in range(2):
+				rawData, image_num, idx = readFile(device, idx)
+				x, y, z = np.array([[],[],[]])
+				for data in rawData:
+					x_new, y_new, z_new = np.transpose(data)
+					x = np.hstack([x, x_new])
+					y = np.hstack([y, y_new])
+					z = np.hstack([z, z_new])
+					scalars = -y
+					plt.mlab_source.reset(x=x, y=y, z=z, scalars=scalars)
+					mlab.title('device '+str(device))
+					yield
+				time.sleep(1)
+			
+			data = align_cloud.alignPointCloud(idx)
+			x, y, z = np.transpose(data)
+			scalars = -y
+			plt.mlab_source.reset(x=x, y=y, z=z, scalars=scalars)
+			mlab.title('aligned device')
+			yield
+			time.sleep(2)
+			
 			idx += 1
 
-
+	print "Start ploting ..."
 	anim(idx)
 	mlab.view(180, 180)
 	mlab.show(stop=True)
